8c90ab69febb8d6727d5f23f0e084c68cf993b62
wanjiru121 <rosewanjiru121@gmail.com>
2019-07-09 19:37:17 +0300

    heroku deploy

--- project2/Akirachix/Akirachix/settings.py
+++ project2/Akirachix/Akirachix/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for Akirachix project.
 
@@ -11,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +128,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'student/media')
+
+
+django_heroku.settings(locals())
